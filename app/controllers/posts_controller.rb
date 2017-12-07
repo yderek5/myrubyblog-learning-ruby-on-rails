1367b68d5a059ae2feced7990e4242319d2e0970
yderek5 <yderek5@gmail.com>
2017-12-06 21:14:42 -0600

    add database stuff

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = 'This is some sample text for our awesome new Ruby blog'
+    @content_second = 'This is some sample text for our awesome new Ruby blog'
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171207030938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "category_id"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
